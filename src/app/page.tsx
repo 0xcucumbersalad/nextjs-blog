c3aa8d5571d36a6e82528ae9564a979558eb509e
Jaynel Patiarba <lenyajpatiarba@gmail.com>
2025-08-21 16:15:53 +0800

    added cbbh certification

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -32,6 +32,14 @@ export default function Home() {
             </li>
             <li className="font-bold hover:underline font-mono">
               <Link
+                href="https://www.credly.com/badges/e8c3b15a-384f-40b7-9446-c622d2587539/public_url"
+                target="_blank"
+              >
+                Hack The Box Certified Bug Bounty Hunter (HTB CBBH)
+              </Link>
+            </li>
+            <li className="font-bold hover:underline font-mono">
+              <Link
                 href="https://certified.tcm-sec.com/066656eb-9683-4f1e-8849-78c9df9da918?key=c340ffe8dd7593e5551d0b555c92f4bd77232eb7b930f199bb0cfe6b11a39655&record_view=true"
                 target="_blank"
               >
@@ -40,18 +48,18 @@ export default function Home() {
             </li>
             <li className="font-bold hover:underline font-mono">
               <Link
-                href="https://www.credly.com/badges/c349d6cf-b854-45a6-b681-8ca9b9a91e9f/public_url"
-                target="_blank"
-              >
-                APIsec Certified Practitioner (ACP)
-              </Link>
-            </li>
-            <li className="font-bold hover:underline font-mono">
-              <Link
                 href="https://candidate.speedexam.net/certificate.aspx?SSTATE=am4131EniU8ntjp4bO5mXXtPW8OUV1g3TyvLhktjKQoZoIntbPUOuhvehyk3j/wFGvfz2CgxISVYTLZ0ZrOnQAds2dufdH55MbWAwRh51f8="
                 target="_blank"
               >
                 Certified AppSec Pentesting eXpert (CAPenX)
+              </Link>
+            </li>
+            <li className="font-bold hover:underline font-mono">
+              <Link
+                href="https://www.credly.com/badges/c349d6cf-b854-45a6-b681-8ca9b9a91e9f/public_url"
+                target="_blank"
+              >
+                APIsec Certified Practitioner (ACP)
               </Link>
             </li>
             <li className="font-bold hover:underline font-mono">
